c6c2a6f81cdfd31d14c99632ec6c92ade94af69c
RUSsiljoe <russiljoe@mail.ru>
2018-12-10 18:31:29 +0300

    Внес некоторые исправления

--- mailer/smart.php
+++ mailer/smart.php
@@ -1,14 +1,8 @@
 <?php
-
 if (
-	mail (
-		"russiljoe@mail.ru", 
-		"Text Ajax", 
-		$_REQUEST["fio"] . " " . $_REQUEST["phone"]
-	)
+	mail("russiljoe@gmail.com", "Text Ajax", $_REQUEST["fio"] . " " . $_REQUEST["phone"])
 )
 	echo "1";
 else 
 	echo "0";
-	
 ?>
